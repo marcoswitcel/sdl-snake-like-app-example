2ef9e504e78a042bfd19011649d1e6225e8277b1
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-06-21 23:43:00 -0300

    primeira flag

--- main.cpp
+++ main.cpp
@@ -670,16 +670,29 @@ void update(Context_Data *context)
   Snake_Entity &snake = context->snake;
   Vec2<unsigned> new_head_position = compute_next_snake_position(context);
   bool is_heading_space_available = is_next_position_valid(context, new_head_position);
+  bool collided_with_border = (snake.dir == LEFT && snake.head.x == 0) ||
+    (snake.dir == RIGHT && snake.head.x == context->arena.width - 1) ||
+    (snake.dir == UP && snake.head.y == 0) ||
+    (snake.dir == DOWN && snake.head.y == context->arena.height - 1);
 
-  if ((snake.dir == LEFT && snake.head.x == 0) ||
-      (snake.dir == RIGHT && snake.head.x == context->arena.width - 1) ||
-      (snake.dir == UP && snake.head.y == 0) ||
-      (snake.dir == DOWN && snake.head.y == context->arena.height - 1)||
-      !is_heading_space_available)
+  // @todo João, falta detectar colisão com paredes num geral e com o próprio corpo
+  // @todo João, outro ponto, esse conjunto de ifs controla se o fluxo deve ser interrompido
+  // e se o estado deve ser mudado para game_over, mas são questões diferentes, talvez fosse
+  // melhor separar.
+  if (collided_with_border || !is_heading_space_available)
   {
     if (snake.dir != NONE)
     {
-      context->state = GAME_OVER;
+      if (collided_with_border)
+      {
+        if (context->arena.loose_condition & LOOSE_ON_HIT_BORDERS)
+        {
+          context->state = GAME_OVER;
+        }
+      } else if (!is_heading_space_available)
+      {
+        context->state = GAME_OVER;
+      }
     }
     return;
   }
