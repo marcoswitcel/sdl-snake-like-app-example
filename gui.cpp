9b5166652e65028fbd698b08383acab04fdcad73
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-07-02 16:12:49 -0300

    renderizando botões em sequência com espaçamento uniforme

--- gui.cpp
+++ gui.cpp
@@ -61,6 +61,17 @@ inline Button create_button(const char *text, SDL_Rect target_area, SDL_Color ba
   };
 }
 
+inline void set_button_to_initial_state(Button *button, const char *text, SDL_Rect target_area, SDL_Color background_color, SDL_Color highlight_background_color)
+{
+  button->text = text;
+  button->hover = false;
+  button->active = false;
+  button->target_area = target_area;
+  button->background_color = background_color;
+  button->highlight_background_color = highlight_background_color;
+  button->timestamp_last_updated = 0;
+}
+
 inline SDL_Color color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 {
   return { .r = r, .g = g, .b = b, .a = a, };
